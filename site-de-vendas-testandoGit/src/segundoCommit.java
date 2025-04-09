fa17286b191f50a67448a59f098cfeed2eed0b46
Winged Warlock <Winged Warlock@DESKTOP-SED08UJ>
2025-04-08 21:47:24 -0300

    Esse é o segundo commit

---
+++ site-de-vendas-testandoGit/src/segundoCommit.java
@@ -0,0 +1,6 @@
+
+public class segundoCommit {
+public static void main(String[] args) {
+	System.out.println("Segundo commit também");
+}
+}
--- site-de-vendas-testandoGit/src/testandoGit.java
+++ site-de-vendas-testandoGit/src/testandoGit.java
@@ -2,5 +2,6 @@
 public class testandoGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Universe, está é a primeira versão deste teste");
+		System.out.println("Agora atualizando o Hello Universe, esse será a partir do segundo commit");
 	}
 }
